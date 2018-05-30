3b47dd2ae68b354e2beb79a2d8d9dc1c9987ff48
Edison E. Abreu <dinho.abreu@gmail.com>
2018-05-30 18:31:21 -0300

    Training packages

---
+++ 02-package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dinhoabreu/go-training/02-package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf("Hello, %s\n", stringutil.MyName)
+}
---
+++ 02-package/stringutil/reverse.go
@@ -0,0 +1,6 @@
+package stringutil
+
+// Reverse reverse string
+func Reverse(s string) string {
+	return reverseTwo(s)
+}
