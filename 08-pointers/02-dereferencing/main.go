d96951c5632dfc3decb99516990696fdce988ae1
Edison E. Abreu <dinho.abreu@gmail.com>
2018-06-15 18:46:07 -0300

    Add pointers

---
+++ 08-pointers/02-dereferencing/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 43
+
+	fmt.Println(a)  // 43
+	fmt.Println(&a) // 0xc420012118
+
+	var b = &a
+	fmt.Println(b)  // 0xc420012118
+	fmt.Println(*b) // 43
+
+	// b is an int pointer;
+	// b points to the memory address where an int is stored
+	// to see the value in that memory address, add a * in front of b
+	// this is known as dereferencing
+	// the * is an operator in this case
+}
